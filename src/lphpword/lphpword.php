039f3fb83be3c914c51d72848b8e6e23b035492b
bennybian <bzh99@hotmail.com>
2017-06-22 15:51:00 -0400

    function createFromTemplate
    createFromTemplate finished.

--- src/lphpword/lphpword.php
+++ src/lphpword/lphpword.php
@@ -1,67 +1,148 @@
-<?php namespace lphpword;
+<?php
+namespace lphpword;
 
 /**
  *
  * Laravel wrapper for PHPWord
- *
- * @category   Laravel Excel
- * @version    1.0.0
- * @package    maatwebsite/excel
- * @copyright  Copyright (c) 2013 - 2014 Maatwebsite (http://www.maatwebsite.nl)
- * @author     Maatwebsite <info@maatwebsite.nl>
- * @license    http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt    LGPL
+ Laravel PHP word -Lphpword
+ 
+
  */
 class Lphpword {
 
-    /**
-     * Filter
-     * @var array
-     */
-    protected $filters = array(
-        'registered' =>  array(),
-        'enabled'    =>  array()
-    );
+	
+	
+	public
 
-    /**
-     * Excel object
-     * @var PHPExcel
-     */
-    protected $excel;
+	function __construct() {
+	
 
-    /**
-     * Reader object
-     * @var LaravelExcelReader
-     */
-    protected $reader;
+	}
+	/*
+	
+	Create file from Template: Only single-line values can be replaced.
+	You can create an OOXML document template with included search-patterns (macros) which can be replaced by any value you wish. Only single-line values can be replaced.
 
-    /**
-     * Writer object
-     * @var LaravelExcelWriter
-     */
-    protected $writer;
+	To deal with a template file, use new TemplateProcessor statement. After TemplateProcessor instance creation the document template is copied into the temporary directory. Then you can use TemplateProcessor::setValue method to change the value of a search pattern. The search-pattern model is: ${search-pattern}.
+	
+    ref:http://phpword.readthedocs.io/en/latest/templates-processing.html
 
-    /**
-     * Construct Excel
-     * @param  PHPExcel           $excel
-     * @param  LaravelExcelReader $reader
-     * @param  LaravelExcelWriter $writer
-     */
-    public function __construct(PHPExcel $excel, LaravelExcelReader $reader, LaravelExcelWriter $writer)
-    {
-        // Set Excel dependencies
-        $this->excel = $excel;
-        $this->reader = $reader;
-        $this->writer = $writer;
-		echo "loaded";
-    }
+	*/
+	public
 
-    /**
-     * Create a new file
-     * @param                $filename
-     * @param  callable|null $callback
-     * @return LaravelExcelWriter
-     */
-    public function create($filename, $callback = null)
-    {}
+	function createFromTemplate( $fillFields = array(), $templateFileName = "", $outputFilename = "" ) {
 
-  }
+
+		if ( !$fillFields || !$templateFileName || !$outputFilename ) {
+			return false;
+		}
+		$doctemplatefolder = \config::get( "app.doctemplatefolder" );
+		$template = new\ PhpOffice\ PhpWord\ TemplateProcessor( $doctemplatefolder . $templateFileName );
+
+		foreach ( $fillFields as $fKey => $fVal ) {
+			$template->setValue( $fKey, $fVal );
+		}
+
+
+		// save as a random file in temp file. Ex: /tmp/PHPWordlNtbA8 
+		$temp_file = tempnam( sys_get_temp_dir(), 'PHPWord' );
+		$template->saveAs( $temp_file );
+
+		$this->savefile( $outputFilename, $temp_file );
+		return;
+
+	}
+	/**
+	 * Create a new file. 
+	 TODO: fill text with variable 
+	
+	 */
+	public
+
+	function create( $outputFilename ) {
+
+		$phpWord = new\ PhpOffice\ PhpWord\ PhpWord();
+		/* Note: any element you append to a document must reside inside of a Section. */
+
+		// Adding an empty Section to the document...
+		$section = $phpWord->addSection();
+		// Adding Text element to the Section having font styled by default...
+		$section->addText(
+			'"Learn from yesterday, live for today, hope for tomorrow. '
+			. 'The important thing is not to stop questioning." '
+			. '(Albert Einstein)'
+		);
+
+		/*
+		 * Note: it's possible to customize font style of the Text element you add in three ways:
+		 * - inline;
+		 * - using named font style (new font style object will be implicitly created);
+		 * - using explicitly created font style object.
+		 */
+
+		// Adding Text element with font customized inline...
+		$section->addText(
+			'"Great achievement is usually born of great sacrifice, '
+			. 'and is never the result of selfishness." '
+			. '(Napoleon Hill)',
+			array( 'name' => 'Tahoma', 'size' => 10 )
+		);
+
+		// Adding Text element with font customized using named font style...
+		$fontStyleName = 'oneUserDefinedStyle';
+		$phpWord->addFontStyle(
+			$fontStyleName,
+			array( 'name' => 'Tahoma', 'size' => 10, 'color' => 'a3402a', 'bold' => true )
+		);
+		$section->addText(
+			'"The greatest accomplishment is not in never falling, '
+			. 'but in rising again after you fall." '
+			. '(Vince Lombardi)',
+			$fontStyleName
+		);
+
+
+
+
+		// save as a random file in temp file. Ex: /tmp/PHPWordlNtbA8 
+
+		$temp_file = tempnam( sys_get_temp_dir(), 'PHPWord' );
+
+		// Saving the document as OOXML file...
+		$objWriter = \PhpOffice\ PhpWord\ IOFactory::createWriter( $phpWord, 'Word2007' );
+		$objWriter->save( $temp_file );
+
+		$this->savefile( $outputFilename, $temp_file );
+
+		//header("Content-Disposition: attachment; filename='myFile.docx'");
+
+
+		return;
+
+	}
+	private
+	function savefile( $outputFilename, $temp_file ) {
+		// Doc generated on the fly, may change so do not cache it; mark as public or
+		// private to be cached.
+		header( 'Pragma: no-cache' );
+		// Mark file as already expired for cache; mark with RFC 1123 Date Format up to
+		// 1 year ahead for caching (ex. Thu, 01 Dec 1994 16:00:00 GMT)
+		header( 'Expires: 0' );
+		// Forces cache to re-validate with server
+		header( 'Cache-Control: must-revalidate, post-check=0, pre-check=0' );
+		// DocX Content Type
+		header( 'Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document' );
+		// Tells browser we are sending file
+		header( 'Content-Disposition: attachment; filename=' . $outputFilename . ';' );
+		// Tell proxies and gateways method of file transfer
+		header( 'Content-Transfer-Encoding: binary' );
+		// Indicates the size to receiving browser
+		header( 'Content-Length: ' . filesize( $temp_file ) );
+
+		readfile( $temp_file ); // or echo file_get_contents($temp_file);
+		unlink( $temp_file ); // remove temp file
+		return;
+
+	}
+
+}
